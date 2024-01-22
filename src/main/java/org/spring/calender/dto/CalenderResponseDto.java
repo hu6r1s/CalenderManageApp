ba4e3a9af875074ebf7f6d1131b4c45a353bb225
hu6r1s <darmaa333@gmail.com>
2024-01-22 20:24:30 +0900

    feat: create calendar

--- src/main/java/org/spring/calender/dto/CalenderResponseDto.java
+++ src/main/java/org/spring/calender/dto/CalenderResponseDto.java
@@ -1,6 +1,8 @@
 package org.spring.calender.dto;
 
+import java.time.LocalDateTime;
 import lombok.Getter;
+import org.spring.calender.entity.Calender;
 
 @Getter
 public class CalenderResponseDto {
@@ -9,5 +11,16 @@ public class CalenderResponseDto {
     private String content;
     private String personCharge;
     private String password;
+    private LocalDateTime createdAt;
+    private LocalDateTime modifiedAt;
 
+    public CalenderResponseDto(Calender calender) {
+        this.id = calender.getId();
+        this.title = calender.getTitle();
+        this.content = calender.getContent();
+        this.personCharge = calender.getPersonCharge();
+        this.password = calender.getPassword();
+        this.createdAt = calender.getCreatedAt();
+        this.modifiedAt = calender.getModifiedAt();
+    }
 }
--- src/main/java/org/spring/calender/entity/Calender.java
+++ src/main/java/org/spring/calender/entity/Calender.java
@@ -6,10 +6,15 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.spring.calender.dto.CalenderRequestDto;
 
 @Entity
+@Getter
+@NoArgsConstructor
 @Table(name="calender")
-public class Calender {
+public class Calender extends Timestamped {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -20,9 +25,16 @@ public class Calender {
     @Column(name = "content", nullable = false)
     private String content;
 
-    @Column(name = "personCharge", nullable = false)
+    @Column(name = "person_charge", nullable = false)
     private String personCharge;
 
     @Column(name = "password", nullable = false)
     private String password;
+
+    public Calender(CalenderRequestDto requestDto) {
+        this.title = requestDto.getTitle();
+        this.content = requestDto.getContent();
+        this.personCharge = requestDto.getPersonCharge();
+        this.password = requestDto.getPassword();
+    }
 }
